6295a05bf1f1fdda0c15d340dcf202b925a6d0ff
chief <zhaoyuu@yonyou.com>
2016-07-18 10:57:47 +0800

    fix:忽略build

--- static/js/template.js
+++ static/js/template.js
@@ -8,9 +8,7 @@ define('template',[],function(){
        var container = options.container;
        var id = options.id+".html" || "template.html";
 
-
        var template =  require('html!../../static/page/'+id);
-
 
        $(container).html(template);
    };
