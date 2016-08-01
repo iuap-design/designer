e1df35cb54244b08e9df09a41404e5d7824043e1
ahua52 <1468492018@qq.com>
2016-08-01 18:44:39 +0800

    图文并茂 数据配置

--- static/js/widget.js
+++ static/js/widget.js
@@ -6,14 +6,13 @@ define('widget',[],function(){
     var init = function (el,name){
 
         $.getJSON('../page/widgetdata/'+name+'.json',function(widgetViewModel){
-            
+            console.log(widgetViewModel);
             var viewModel = {
                 carousels: ko.observableArray(widgetViewModel)
             }
             ko.applyBindings(viewModel,el);  
             if(name == "widget7"){
                 setTimeout(function(){
-                    console.log($('#carousel-example-generic'));
                     $('#carousel-example-generic').carousel();
                 }, 5000 );
             }
