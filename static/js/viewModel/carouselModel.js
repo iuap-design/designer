5651134cb9cd033090d2a3e0a0fc84e875294d69
ahua52 <1468492018@qq.com>
2016-08-02 19:30:50 +0800

    ko绑定属性面板 且添加新的操作数据

--- static/js/viewModel/carouselModel.js
+++ static/js/viewModel/carouselModel.js
@@ -4,19 +4,19 @@
 
 define('widget',[],function(){
     var data = [{
-        "carouselName": "carousel-1",
+        "carouselName": ko.observable("carousel-1"),
         "carouselImg": "http://design.yyuap.com/static/img/carousel-1.jpg",
         "carouselNameColor": "#fff",
         "carouselNameSize": "12px"
       },
       {
-        "carouselName": "carousel-2",
+        "carouselName": ko.observable("carousel-2"),
         "carouselImg": "http://design.yyuap.com/static/img/carousel-2.jpg",
         "carouselNameColor": "#fff",
         "carouselNameSize": "12px"
       },
       {
-        "carouselName": "carousel-3",
+        "carouselName": ko.observable("carousel-3"),
         "carouselImg": "http://design.yyuap.com/static/img/carousel-3.jpg",
         "carouselNameColor": "#fff",
         "carouselNameSize": "12px"
@@ -24,12 +24,20 @@ define('widget',[],function(){
 
     var viewModel = function(data) {
         this.carousels = ko.observableArray(data);
-        // this.addItem = function() {
-        //     if (this.itemToAdd() != "") {
-        //         this.items.push(this.itemToAdd()); 
-        //         this.itemToAdd(""); 
-        //     }
-        // }.bind(this);  
+      
+        this.addCarousel = function() {
+            var newCarouselItem = {
+                "carouselName": ko.observable("carousel-4"),
+                "carouselImg": "http://design.yyuap.com/static/img/carousel-2.jpg",
+                "carouselNameColor": "#fff",
+                "carouselNameSize": "12px"
+            };
+            this.carousels.push(newCarouselItem); 
+        }.bind(this); 
+
+        this.deleteCarousel = function(item) {
+            this.carousels.remove(item)
+        }.bind(this);  
     };
 
     return new viewModel(data)
