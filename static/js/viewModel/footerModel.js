988aab951ba483631b63b211c0c813cd721e9b17
ahua52 <1468492018@qq.com>
2016-08-05 16:47:52 +0800

    优化footer

--- static/js/viewModel/footerModel.js
+++ static/js/viewModel/footerModel.js
@@ -4,7 +4,8 @@
 
 define('footer',[],function(){
     var data =[
-        {
+        {   
+            "footerImg": ko.observable("http://design.yyuap.com/static/img/bg-all.jpg"),
             "footerInfo":  ko.observable("版权所有：用友网络科技股份有限公司 2016 京ICP备05007539号-7 京ICP证100714号 京公网安备1101080209224号"),
             "footBackground":"#ededed",
             "footerFontSize": "14px"
