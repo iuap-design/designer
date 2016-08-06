5fbe87ed31bbb3fa33ebcd569b8475809171c09f
ahua52 <1468492018@qq.com>
2016-08-06 16:33:18 +0800

    修改导航名字

--- static/js/viewModel/headNavModel.js
+++ static/js/viewModel/headNavModel.js
@@ -8,20 +8,20 @@ define('headNav',[],function(){
           "backgroudColor": "#FD9C9C",
           "navlist": [
               {
-                "title": ko.observable("班级首页"),
+                "title": ko.observable("赛事日程"),
                 "titleColor": "#fff",
                 "titleSize": "14px"
               },{
-                "title": ko.observable("班级相册"),
+                "title": ko.observable("走进里约"),
                 "titleColor": "#fff",
                 "titleSize": "14px"
               },
               {
-                "title": ko.observable("班级成员"),
+                "title": ko.observable("运动员风采"),
                 "titleColor": "#fff",
                 "titleSize": "14px"
               },{
-                "title": ko.observable("下载专区"),
+                "title": ko.observable("直播专区"),
                 "titleColor": "#fff",
                 "titleSize": "14px"
               }
@@ -36,7 +36,7 @@ define('headNav',[],function(){
 
         this.addItem = function() {
             var newheadNavItem =  {
-                "title": ko.observable("班级成员"),
+                "title": ko.observable("热点快报"),
                 "titleColor": "#fff",
                 "titleSize": "14px"
               };
