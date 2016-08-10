cc3fee10c045252d02d211286a6c1913698bee1d
chief <zhaoyuu@yonyou.com>
2016-08-10 14:20:46 +0800

    fix:test

--- static/js/template.js
+++ static/js/template.js
@@ -24,8 +24,9 @@ define('template', ['./widget'], function (widget) {
                 if(options.id=='liyue'){
                     var elements = $(container).find('.u-widget');
                     $.each(elements,function(i,item){
-                        console.log(elements);
-                        widget.init(item[0],$(item).attr('panelname'));
+                        console.log(item[0]);
+                        console.log($(item).attr('panelname'));
+                        //widget.init(item[0],$(item).attr('panelname'));
                     });
                 }
             }
