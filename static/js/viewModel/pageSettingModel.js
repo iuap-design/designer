e5f3c5ce312386e5fd86f7262f95307a82f8425c
chief <zhaoyuu@yonyou.com>
2016-08-18 16:18:15 +0800

    fix:sidebar样式修改

--- static/js/viewModel/pageSettingModel.js
+++ static/js/viewModel/pageSettingModel.js
@@ -9,9 +9,13 @@ define('pageSetting',[],function(){
     }];
 
     var badgeModel = function() {
-        this.bgColor = ko.observable('rgba(0, 188, 212, 0.05)');
-        this.bgGround = ko.observable('');
-        this.bgRepeat = ko.observable(true);
+        this.bgColor = ko.observable('#ffffff');
+        this.bgGround = ko.observable();
+        this.bgSize = ko.observable('');
+        var self = this;
+        this.bgImageUrlStyle = ko.computed(function() {
+            return "url(" + self.bgGround() + ")";
+        });
     };
 
     return new badgeModel();
