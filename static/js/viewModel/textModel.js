7bed8ac7cacdda2785ed79ea52f82132585af9a2
ahua52 <1468492018@qq.com>
2016-09-02 14:46:23 +0800

    Text 添加link

--- static/js/viewModel/textModel.js
+++ static/js/viewModel/textModel.js
@@ -8,18 +8,21 @@ define('text',[],function(){
             "fontColor":ko.observable("#000"),
             "fontWeight": ko.observable("600"),
             "text": ko.observable("我是标题"),
+            "linkurl": null,
             "fontSize": ko.observable("14")
         },{
             "align":ko.observable("middle"),
             "fontColor":ko.observable("#000"),
             "fontWeight": ko.observable("400"),
+            "linkurl": null,
             "text": ko.observable("基于20多年企业开发经验，总结了企业应用领域中遇到的常见场景，并提供相应的解决方案。企业级应用开发不再困难"),
             "fontSize": ko.observable("14")
         },{
             "align":ko.observable("right"),
             "fontColor":ko.observable("#000"),
             "fontWeight": ko.observable("800"),
-            "text": ko.observable("label"),
+            "text": ko.observable("link"),
+            "linkurl": "http://",
             "fontSize": ko.observable("14")
         }];
     var textModel = function(data) {
