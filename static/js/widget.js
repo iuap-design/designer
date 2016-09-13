3f7ea508baf8b85f6d549eb98e108c1276025c09
ahua52 <1468492018@qq.com>
2016-09-13 19:15:18 +0800

    多实例问题

--- static/js/widget.js
+++ static/js/widget.js
@@ -13,8 +13,15 @@ define('widget',[],function(){
           
         var widgetViewModel = require('./viewModel/'+name+'Model.js');
 
+        var widgetViewModelClone = widgetViewModel;
+
+        $(el).data("model",widgetViewModelClone);
+
+        ko.applyBindings(widgetViewModelClone,el);
+
         
-        ko.applyBindings(widgetViewModel,el); 
+
+        //ko.applyBindings(widgetViewModel,el); 
         
         // 导航条 当手机屏幕 icon触发导航条显示
         $(".nav-toggle").unbind("click").bind("click",function(){
