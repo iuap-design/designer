29f88b9ad355793191486bec4affa78862065e41
wangbow <234211172@qq.com>
2016-09-14 12:29:52 +0800

    updata card.html

---
+++ static/js/viewModel/cardModel.js
@@ -0,0 +1,28 @@
+/**
+ * Created by chief on 16/7/16.
+ */
+
+define('card',[],function(){
+    var data = [{
+        "bgColor":ko.observable(""),
+        "text": ko.observable("60")
+
+    }
+    ];
+
+    var cardModel = function(data) {
+        this.cardDate = ko.observableArray(data);
+
+        //this.changeBgColor = function(){
+        //    var index = $(event.target).parents("[index]").attr("index");
+        //    alert($(event.target).val());
+        //    //this.cardDate()[index].bgColor(bgColor);
+        //}.bind(this);
+
+    };
+
+    return new cardModel(data);
+
+
+
+});
--- static/js/viewModel/imageModel.js
+++ static/js/viewModel/imageModel.js
@@ -5,20 +5,36 @@
 define('image',[],function(){
     var data = [{
             "imageUrl":ko.observable("http://design.yyuap.com/static/img/designer/sidebar/img_cd.jpg"),
-            "borderWidth": ko.observable("1px"),
-            "borderColor": ko.observable("#000"),
-            "borderRadius": ko.observable("0px"),
-            "borderStyle": ko.observable("solid")
+            "imgWidth": ko.observable("img-responsive"),
+             "borderRadius": ko.observable("4px"),
+             "size": ko.observable("sm"),
+             "borderStyle":ko.observable("")
         },{
             "imageUrl":ko.observable("http://design.yyuap.com/static/img/designer/sidebar/img_cd.jpg"),
-            "borderWidth": ko.observable("1px"),
-            "borderColor": ko.observable("#fff"),
-            "borderRadius": ko.observable("10px"),
-            "borderStyle": ko.observable("solid")
+            "imgWidth": ko.observable("img-responsive"),
+            "borderRadius": ko.observable("4px"),
+              "size": ko.observable("sm"),
+            "borderStyle":ko.observable("")
         }];
 
     var imageModel = function(data) {
         this.image = ko.observableArray(data);
+
+        this.changeImgWidth = function(){
+            var index = $(event.target).parents("[index]").attr("index");
+            var imgWidtg = $(event.target).find("option:selected").val() + " img-responsive";
+            this.image()[index].imgWidth(imgWidtg);
+        }.bind(this);
+        this.changeBorderRadius = function(size){
+            var index = $(event.target).parents("[index]").attr("index");
+            this.image()[index].borderRadius(size);
+        }.bind(this);
+        this.changeImgBorderColor = function(){
+            var index = $(event.target).parents("[index]").attr("index");
+            var imgBorderColor = $(event.target).find("option:selected").val().substring(9);
+            var jointBorderColor="1px solid "+imgBorderColor ;
+            this.image()[index].borderStyle(jointBorderColor);
+        }.bind(this);
     };
 
     return new imageModel(data);
